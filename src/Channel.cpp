62b1fce817cac4240f78166787fceac3142de767
Claire Reyt <clairereyt@MacBook-Pro-de-Claire.local>
2023-07-20 10:08:38 +0200

    channel base

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -24,12 +24,13 @@ std::string	Channel::getNameChannel()
     return (_nameChannel);
 }
 
-// void Channel::addMembers(Client *client)
-// {
-//     if (client->isAuthenticated())
-// 		_members.push_back(client);
-// 	else {
+ void Channel::addMembers(Client *client)
+{
+    if (client->isAuthenticated())
+		// _members.push_back(client);
+        std::cout << "new member" << std::endl;
+    else {
 		
-// 		return;
-// 	}
-// }
+		return;
+	}
+}
