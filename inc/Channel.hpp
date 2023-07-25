c7ccacfcbb26316f68579f1bdfc0c5b97f9b845a
nchennaf <nchennaf@student.42lausanne.ch>
2023-07-25 17:40:23 +0200

    removeMember added

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -14,6 +14,7 @@ class Channel
 			~Channel();
 
 			void						addMember(Client *client);
+			void						removeMember(Client *client, int fd);
 			void						setTopic(std::string topic);
 			void						sendToAllMembers(std::string msg);
 
